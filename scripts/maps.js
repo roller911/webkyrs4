6169a5d50be5dde68ba06cf059114c131c5556c5
roller911 <roller911@yadnex.rugit config --global user.name roller911git commit -m >
2024-10-21 20:16:40 +0300

    Зашаманил карту

--- scripts/maps.js
+++ scripts/maps.js
@@ -4,21 +4,16 @@
         function init() {
             var mapDiv = document.getElementById("map");
             var myMap = new ymaps.Map(mapDiv, {
-                center: [56.32, 44],
-                zoom: 7
+                center: [56.326552, 44.024662],
+                zoom: 14
             });
 
-            // Добавьте маркер
-            var latlong = [56.2863, 44.0836];
-            var myPlacemark = new ymaps.Placemark(latlong, {
-                hintContent: 'Корпус №6 НГТУ им. Р.Е. Алексеева',
-                balloonContent: 'Корпус №6 НГТУ им. Р.Е. Алексеева'
-            }, {
-                iconLayout: 'default#image',
-                iconImageHref: 'https://yandex.ru/mapsmarker/marker_00000.png',
-                iconImageSize: [12, 12],
-                iconImageOffset: [-3, -12]
-            });
-
-            myMap.geoObjects.add(myPlacemark);
-        }
+            myMap.geoObjects
+            .add(new ymaps.Placemark([56.326552, 44.024662], {
+                 hintContent: 'Корпус №1 НГТУ им. Р.Е. Алексеева',
+                 balloonContent: 'Корпус №1 НГТУ им. Р.Е. Алексеева'
+             }, {
+            preset: 'islands#dotIcon',
+            iconColor: '#735184' 
+        }));
+}
