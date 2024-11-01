e3c52bd312a44452002a50d7964fcc6317900010
roller911 <roller@yandex.ru>
2024-11-01 12:58:46 +0300

    Зачем володя сбрил усы?

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,25 +1,45 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    const commentContainer = document.querySelector('.comments');
-    const nameInput = document.getElementById('controlinput1');
-    const commentInput = document.getElementById('controltextarea1');
-    const submitButton = document.querySelector('.submit-comment');
+ document.addEventListener('DOMContentLoaded', function() {
+            const commentContainer = document.querySelector('.comments');
+            const nameInput = document.getElementById('controlinput1');
+            const commentInput = document.getElementById('controltextarea1');
+            const submitButton = document.getElementById('submit-comment');
+            const timeInput = 4000;
 
-    submitButton.addEventListener('click', function(e) {
-      e.preventDefault();
-      const name = nameInput.value;
-      const comment = commentInput.value;
+            submitButton.addEventListener('click', function(e) {
+                e.preventDefault();
+                const name = nameInput.value;
+                const comment = commentInput.value;
 
-      if (name && comment) {
-        const commentHTML = `
-          <div class="comment">
-            <h4>${name}</h4>
-            <p>${comment}</p>
-          </div>
-        `;
+                if (name && comment) {
+                    submitButton.classList.add("onclic");
 
-        commentContainer.insertAdjacentHTML('beforeend', commentHTML);
-        nameInput.value = '';
-        commentInput.value = '';
-      }
-    });
-  });
+                    function validate() {
+                        setTimeout(function() {
+                            submitButton.classList.remove("onclic");
+                            submitButton.classList.add("validate");
+
+                            function callback() {
+                                setTimeout(function() {
+                                    submitButton.classList.remove("validate");
+                                }, 2000);
+                            }
+                            callback();
+                        }, timeInput);
+                    }
+
+                    validate();
+
+                    setTimeout(function() { 
+                      const commentHTML = `
+                        <div class="comment">
+                            <h4>${name}</h4>
+                            <p>${comment}</p>
+                        </div>
+                    `;
+                    commentContainer.insertAdjacentHTML('beforeend', commentHTML);
+                    nameInput.value = '';
+                    commentInput.value = '';
+                  },timeInput);
+                }
+            });
+        });
